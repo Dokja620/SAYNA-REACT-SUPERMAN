e193009d6238adaa0288155172b771eb8f949690
workja <revanamike620@gmail.com>
2023-10-27 14:52:01 +0300

    Trying to fix links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import Navbar from './shared/navbar'
-import Footer from './shared/footer'
+import Navbar from './shared/navbar';
+import Footer from './shared/footer';
 
 import Home from './pages/home';
 import Eshop from './pages/eshop';
@@ -15,19 +15,19 @@ import PasswordRecovery from './pages/sub-account/recovery';
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-        <Navbar/>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path='home' element={<Home/>}/>
-            <Route path='e-shop' element={<Eshop/>}/>
-            <Route path='account' element={<Account/>}/>
-            <Route path='infos' element={<Infos/>}/>
-            <Route path="Registration" element={<Registration/>}/>
-            <Route path="Dashboard" element={<Dashboard/>}/>
-            <Route path="recovery" element={<PasswordRecovery/>}/>
-          </Routes>
-        <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/e-shop" element={<Eshop />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/infos" element={<Infos />} />
+          <Route path="/Registration" element={<Registration />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/recovery" element={<PasswordRecovery />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
